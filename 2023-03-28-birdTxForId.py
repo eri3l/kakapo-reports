c28f973bcba38f8f1fa8c20bc33755e019c4853b
eri3l <eri3l@users.noreply.github.com>
2023-04-04 14:52:15 +1200

    Quick streamlit

--- 2023-03-28-birdTxForId.py
+++ 2023-03-28-birdTxForId.py
@@ -1,6 +1,10 @@
 # fixing bug in birdTxForId() 
 # given (tx_channel, island, (date)), find bird
 
+'''
+Note: dateTime is the date of the tx off (removed), date on (installed) is the dateTime of the prev row
+'''
+
 import numpy as np
 import pandas as pd
 
@@ -18,8 +22,6 @@ collist = ['harnessChangeOnly', 'newStatus','prox', 'proxOn', 'recoveryID', 'rem
 records_head2 = records_head.dropna(subset=['txFrom'])  # NB! txFrom can be NA
 records_head3 = records_head.dropna(subset=['txFrom', 'txTo'], how='all') 
 # want to check for NA in ALL cols
-roha = records[records['birdName'] == 'Roha']
-roha_tx = roha.dropna(subset=['txFrom', 'txTo'], how='all')
 
 #region txIdForTxUniqueId
 txFrom = "a8db17a5-49d3-4382-b0d8-36d1ee0d5013"
@@ -53,17 +55,11 @@ def getTxId3(tx_unique):
         return 0    
 # hack end
 
-
 res2 = getTxId("a8db17a5-49d3-4382-b0d8-36d1ee0d5013")
 mytxid = "a8db17a5-49d3-4382-b0d8-36d1ee0d5013"
 mytxid = "89df0830-227f-40fc-a342-46014bb9b25e"
 print("txId for %s is:" % mytxid, getTxId2(mytxid))
 print("txChannel for %s is:" % mytxid, getTxId3(mytxid))
-
-roha_tx['txFromDuskyId'] = roha_tx['txFrom'].apply(getTxId2)    
-roha_tx['txChannel'] = roha_tx['txFrom'].apply(getTxId3)        # FIXME: should really be one function together
-roha_tx['txToDuskyId'] = roha_tx['txTo'].apply(getTxId2)
-roha_tx = roha_tx.sort_values(by='dateTime', ascending=True)
 
 ''' next: for a certain datetime, get transmitter and channel
 date == 16/09/2020
@@ -81,40 +77,68 @@ df[date_columns] = df[date_columns].apply(pd.to_datetime, format='%d/%m/%Y')
 in_between = df.purchaseDate.between(df.releaseDate, df.ceaseDate)
 df['status'] = np.where(in_between, 'Active', 'Inactive')
 '''
-#region find date
-#endregion //find date
 
 #region full records
 records_tx = records.dropna(subset=['txFrom', 'txTo'], how='all')
 
-records_tx = records_tx.sort_values(by='dateTime', ascending=False)
+records_tx = records_tx[['birdID', 'birdName', 'dateTime', 'id', 'island', 'recordType', 
+                         'reason', 'newStatus', 'txDocId', 'txFrom', 'txId', 'txTo']]
+                         #'txFromDuskyId', 'txChannel', 'txToDuskyId']]
+records_tx = records_tx.sort_values(by='dateTime', ascending=False)         # NB! WATCH IT! (txDateOn with pd.shift())
 
 records_tx['txFromDuskyId'] = records_tx['txFrom'].apply(getTxId2)    
 records_tx['txChannel'] = records_tx['txFrom'].apply(getTxId3)        # FIXME: should really be one function together
 records_tx['txToDuskyId'] = records_tx['txTo'].apply(getTxId2)
 
+#endregion // full records
+
+#region find date
+'''
+Note: dateTime is the date of the tx off (removed), date on (installed) is the dateTime of the prev row
+make two cols, dateOn (dateTxInstalled) and dateOff (dateTxRemoved)
+'''
+# NB! df.shift(i) shifts the entire dataframe by i units DOWN. Shift(-1) to shift UP.  WATCH WATCH WATCH
+
+# records_tx = records_tx[['birdName', 'dateTime', 'island', 'txFromDuskyId', 'txChannel']
+records_tx['txDateOff'] = records_tx['dateTime']
+
+# something with groupby and shift?
+records_tx['txDateOn'] = records_tx.groupby('birdName')['dateTime'].shift(-1)
+# TODO: really need a test/assertion here to make sure txDateOn is correct
+
 # write to csv so you don't do this every time
 # records_tx.to_csv("31-03-2023-records_tx.csv", sep=',', index=False)
 # records_new = pd.read_csv("31-03-2023-records_tx.csv")
 
-# test cases
+#endregion //find date
+
+#region test cases
+orion = records_tx[records_tx['birdName'] == 'Orion']
+orion = orion[['birdName', 'dateTime', 'island', 'txFromDuskyId', 'txChannel', 'txDateOn', 'txDateOff']]
+
+
 ## 1. given a txDuskyId, return list of all records for it
 myTxDuskyId = "1149"
-tempres = records_tx[records_tx['txFromDuskyId'] == myTxDuskyId]
-tempres['birdName'].values[0]
-tempres[['birdName', 'island', 'dateTime']].values[0]
-tempres.iloc[0]['birdName']
+resCase1 = records_tx[records_tx['txFromDuskyId'] == myTxDuskyId]
+resCase1['birdName'].values[0]
+resCase1[['birdName', 'island', 'dateTime']].values[0]
+resCase1.iloc[0]['birdName']
 
 ## 2. given a txChannel, return list (birdName, date, txid)
 myTxCh = 93
+
+myTxCh = int(input("Search by transmitter channel: "))
+
 chres = records_tx[records_tx['txChannel'] == myTxCh]
+
+chres = chres[['birdName', 'dateTime', 'island', 'txFromDuskyId', 'txChannel']]
 chres['birdName'].values        # array
 chres[['birdName', 'island', 'dateTime']].values
 
 #region print a pretty table
 from tabulate import tabulate
 
-print(tabulate(chres, headers = chres.columns, tablefmt = 'psql'))
+print(tabulate(chres, headers = chres.columns, tablefmt = 'github', showindex=False))  # psql, pretty, jira
 #endregion // print a pretty table
 
 ## 3. given a txChannel (or txDuskyId) and island, return bird list
@@ -123,8 +147,10 @@ resCase3 = records_tx[(records_tx['txChannel'] == myTxCh ) & (records_tx['island
 
 ## 4. given a txChannel (or txDuskyId), island and date return bird list
 
+#endregion // test cases
+
 ''' 
-jupyter ibnteractive widget
+jupyter interactive widget
 publishing notebooks with plotly
 '''
-#endregion // full records
+
---
+++ 2023-04-03-streamlit.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  3 13:27:31 2023
+
+@author: eriel
+"""
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Search for bird by tx channel or id")
+
+#### prep ####
+
+### get data ###
+records_tx = pd.read_csv("dat/processed/31-03-2023-records_tx.csv")
+st.subheader('Manipulated transmitter records')
+st.dataframe(records_tx.head())
+
+#### search ####
+st.subheader('Case 1. Given a txDuskyId, return list of all records for it')
+
+myTxDuskyId = st.text_input("Search by transmitter id: ")
+st.write('Searching by transmitter id: ', myTxDuskyId)
+
+resCase1 = records_tx[records_tx['txFromDuskyId'] == myTxDuskyId]
+resCase1 = resCase1[['birdName', 'txFromDuskyId', 'txChannel','txDateOn', 'txDateOff', 'island']]
+
+st.table(resCase1)
+
+##
+st.subheader('Case 2 . given a txChannel, return list (birdName, date, txid)')
+myTxCh = st.number_input("Search by transmitter channel: ")
+
+resCase2 = records_tx[records_tx['txChannel'] == myTxCh]
+resCase2 = resCase2[['birdName', 'txFromDuskyId', 'txChannel','txDateOn', 'txDateOff', 'island']]
+
+st.write(resCase2)
+
+## 
+st.subheader('Case 3. (island) Given a txChannel or txDuskyId and/or island, return bird list')
+
+myTxChCase3 = st.number_input("Enter transmitter channel: ")
+myIsland = st.text_input("Enter island: ")
+
+# myIsland = "Whenua Hou"
+resCase3 = records_tx[(records_tx['txChannel'] == myTxChCase3 ) & (records_tx['island'] == myIsland)]
+
+resCase3 = resCase3[['birdName', 'txFromDuskyId', 'txChannel','txDateOn', 'txDateOff', 'island']]
+
+st.dataframe(resCase3)
